1c0f695ac49a0ec027aad13ed58ef836dac337b6
Krishna20122008 <kchourasia121@gmail.com>
2026-08-11 18:26:01 +0530

    Pushed IncrementDecrement.c

---
+++ Yt C learning/IncrementDecrement.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+
+    int a = 10; 
+    printf("%d\n", a++); //10
+    printf("%d\n", a); //11
+    printf("%d\n", ++a); //12
+
+    printf("%d\n", a--); //12
+    printf("%d\n", a); //11
+    printf("%d\n", --a); //10
+}
