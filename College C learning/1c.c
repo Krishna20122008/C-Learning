16f2a93c2c17546fb89739d4be54aaee7e0a518e
Krishna20122008 <kchourasia121@gmail.com>
2026-08-17 17:15:39 +0530

    Pushed 1c.c

---
+++ College C learning/1c.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    printf("Demonstration of Escape Sequence characters\n");
+
+    printf("\n1. New Line:\\n\n");
+
+    printf("Hello\nWorld");
+
+    printf("\n\n2. Horizontal Tab:\\t\n");
+
+    printf("Name\tAge\tMarks\n");
+
+    printf("Rahul\t18\t85");
+
+    printf("\n\n3. Backshlash\\\\\n");
+
+    printf("C:\\Users\\Student");
+
+    printf("\n\n4. Double Quote:\\\"\n");
+
+    printf("She said, \"Hello!\"");
+
+    printf("\n\n5. Single Quote:\\'\n");
+
+    printf("\n\n6. Backspace:\\b\n");
+
+    printf("\n\ncarriage return:\\r\n");
+    
+    printf("Hello\rHi");
+}
