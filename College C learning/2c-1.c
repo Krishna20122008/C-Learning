c85986a26538228f6b204841e2a7dfbbd310ea62
Krishna20122008 <kchourasia121@gmail.com>
2026-08-24 16:55:02 +0530

    Edited 2c-1.c

--- College C learning/2c-1.c
+++ College C learning/2c-1.c
@@ -2,8 +2,9 @@
 int main(){
     // Swapping:
 
-    int n1 = 10;
-    int n2 = 20;
+    int n1, n2;
+    printf("Enter the variables one by one: ");
+    scanf("%d %d", &n1, &n2);
     int temp = n1;
 
     printf("The two numbers before swapping are: %d and %d\n", n1, n2);
