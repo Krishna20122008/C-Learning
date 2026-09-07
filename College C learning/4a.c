75f30cdd3a71118aae328677e43da9a476f86d1b
Krishna20122008 <kchourasia121@gmail.com>
2026-09-07 17:16:50 +0530

    Pushed 4a.c

---
+++ College C learning/4a.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the no. of rows: ");
+    scanf("%d", &n);
+
+    for(int i=0; i<n; i++){
+        int ascii = 65+i;
+        for(int j = 65; j<=ascii; j++){
+            printf("%c", (char)j);
+        }
+        printf("\n");
+    }
+}
